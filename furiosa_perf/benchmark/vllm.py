4905f61e7d9fac12672e9d30b75b87472356b60d
jongwook_kim <134344843+jongwook11@users.noreply.github.com>
2026-07-03 23:25:51 +0900

    Fix typo in temperature argument in vllm.py

--- furiosa_perf/benchmark/vllm.py
+++ furiosa_perf/benchmark/vllm.py
@@ -50,7 +50,7 @@ class VllmPerformanceBenchmark:
         "--result-dir={result_dir}",
         "--ready-check-timeout-sec=0",
         "--trust-remote-code",
-        "--temperatrue=0",
+        "--temperature=0",
     ]
 
     VLLM_COMMANDS: dict[str, list[str]] = {
